3f1883316e1e342ac9c8eeb1f793892a0d6c1ea7
zayyid <mochamadzayyid@gmail.com>
2022-05-08 12:58:42 -0700

    preparation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
+import Home from './page/home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <React.Fragment>
+      <Home/>
+    </React.Fragment>
   );
 }
 
